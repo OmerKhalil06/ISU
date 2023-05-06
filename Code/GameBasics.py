9564188fcf1d0ad3d5cedcf101a500b532f13f0a
omer <ok.khalil06@gmail.com>
2023-05-06 16:38:18 -0400

    added animations

--- Code/GameBasics.py
+++ Code/GameBasics.py
@@ -2,7 +2,6 @@
 # 2023-04-29
 # begin the basics of the game
 
-
 import pygame #? import the pygame library 
 import os
 
@@ -15,49 +14,94 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 VELOCITY = 10
 FWIDTH, FHEIGHT = 150, 300 
 
-
 #? images defined 
-#!background = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'cat.test.jpg')), (WIDTH, HEIGHT))
+background = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'castle.png')), (WIDTH, HEIGHT))
+
 blue_fighter = pygame.transform.scale(pygame.image.load(
     os.path.join('Images', 'Blue1.png')), (150, 300))
+blue_stand = pygame.transform.scale(pygame.image.load(
+    os.path.join('Images', 'Blue3.png')), (150, 300))
+blue_punch = pygame.transform.scale(pygame.image.load(
+    os.path.join('Images', 'Blue3.png')), (150, 300))
+
 red_fighter = pygame.transform.flip(pygame.transform.scale(pygame.image.load(
     os.path.join('Images', 'Red1.png')), (150,300)), True, False)
+red_stand = pygame.transform.flip(pygame.transform.scale(pygame.image.load(
+    os.path.join('Images', 'Red3.png')), (150,300)), True, False)
+red_back = pygame.transform.flip(pygame.transform.scale(pygame.image.load(
+    os.path.join('Images', 'Red4.png')), (180,300)), True, False)
 
-def draw_window(blue, red): #? draw window to create images 
-    #!WINDOW.blit(background, (0,0)) #? background is drawn on window 
-    WINDOW.fill((255,255,255))  #todo: add real background later  
+def draw_window(blue, red, blue_frame, red_frame, blue_hp,red_hp, blue_healthbar): #? draw window to create images 
+    blue_anim = [blue_fighter, blue_stand]
+    red_anim = [red_fighter, red_stand, red_back, red_stand]
+    WINDOW.blit(background, (0, 0)) #? background is drawn on window  
+    #! health bars 
+    pygame.draw.rect(WINDOW, (0,0,0), pygame.Rect(0, 0, 1200, 100)) #? black healthbar background    
+    pygame.draw.rect(WINDOW, (255,0,0), pygame.Rect(0,0,500, 100)) 
+    pygame.draw.rect(WINDOW, (0,150,0), pygame.Rect(0,0,red_hp*5, 100))   
+    pygame.draw.rect(WINDOW, (255,0,0), pygame.Rect(700, 0 ,700, 100))  
+    pygame.draw.rect(WINDOW, (0,150,0), pygame.Rect(700 + blue_healthbar, 0, blue_hp*5, 100))   
+
     pygame.draw.rect(WINDOW, (0,255,0), pygame.Rect(0, 600, 1200, 100)) 
-    WINDOW.blit(blue_fighter,(blue.x, blue.y))
-    WINDOW.blit(red_fighter, (red.x, red.y)) #? blit = method to place on screen 
-    
-    
+    WINDOW.blit(blue_anim[blue_frame],(blue.x, blue.y))
+    WINDOW.blit((red_anim[red_frame]), (red.x, red.y)) #? blit = method to place on screen 
     pygame.display.update()
     
 def main():
+    #todo: add damage and attacks 
+    blue_hp = 50
+    red_hp = 100
+    blue_healthbar = (100 - blue_hp)*5
+    #! animations 
+    last_update = pygame.time.get_ticks()
+    blue_animation_cooldown, red_animation_cooldown= 150, 150
+    blue_frame = 0 
+    red_frame = 0
     blue = pygame.Rect(1050,300, FWIDTH, FHEIGHT)
     red = pygame.Rect(0,300, FWIDTH, FHEIGHT)
     frame_rate = pygame.time.Clock()
-    MIDDLE = ((red.x +blue.x)//2)
     run = True 
     while run:
-        
+        #! updating animations 
+        current_time = pygame.time.get_ticks()
         frame_rate.tick(FPS) #? control speed of while loop / second 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_LEFT] and blue.x + VELOCITY > red.x+100:
+        if keys_pressed[pygame.K_LEFT] and blue.x + VELOCITY > red.x+150:
             blue.x -= VELOCITY 
-            #todo: add animations to a list and cycle through them in a while loop 
+            if current_time - last_update >= blue_animation_cooldown:
+                blue_frame +=1 
+                last_update = current_time
+                if blue_frame >= 2:
+                    blue_frame = 0 
         if keys_pressed[pygame.K_RIGHT] and blue.x - VELOCITY!= 1050:
             blue.x += VELOCITY
-        if keys_pressed[pygame.K_d] and red.x + VELOCITY < blue.x - 100:
+            if current_time - last_update >= blue_animation_cooldown:
+                blue_frame +=1 
+                last_update = current_time
+                if blue_frame >= 2:
+                    blue_frame = 0 
+
+        if keys_pressed[pygame.K_d] and red.x + VELOCITY < blue.x - 150:
             red.x += VELOCITY 
+            if current_time - last_update >= red_animation_cooldown:
+                red_frame +=1 
+                last_update = current_time
+                if red_frame >= 4:
+                    red_frame = 0 
+
         if keys_pressed[pygame.K_a] and red.x !=0:
             red.x -= VELOCITY 
+            if current_time - last_update >= red_animation_cooldown:
+                red_frame +=1 
+                last_update = current_time
+                if red_frame >= 4:
+                    red_frame = 0
 
-        draw_window(blue, red)
+        draw_window(blue, red, blue_frame, red_frame, blue_hp, red_hp, blue_healthbar)
         
         
     pygame.quit() #? if loop breaks then exit the pygame
