b60b57b66c92d13029daf91361f5b1e7ac605506
omer <ok.khalil06@gmail.com>
2023-06-06 19:26:50 -0400

    made AI unbeatable + fixed post game texts

--- Code/Pyting Game - Final.py
+++ Code/Pyting Game - Final.py
@@ -2,6 +2,7 @@
 # 2023-04-29
 # Main Game File
 # todo: add way to exit out of main menu
+import time
 import pygame #? import the pygame library 
 import os
 pygame.font.init() #initialize fonts 
@@ -24,8 +25,7 @@ WINNER_FONT = pygame.font.SysFont('VT323', 100)
 #* Images  
 background = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'dojo.jpg')), (WIDTH, HEIGHT))
 
-floor = pygame.transform.scale(pygame.image.load(
-    os.path.join('Images', 'floor.png')), (100,100))
+floor = pygame.transform.scale(pygame.image.load(os.path.join('Images', 'floor.png')), (100,100))
 logo = pygame.transform.scale(pygame.image.load(
     os.path.join('Images', 'logo.png')), (70,100))
 
@@ -139,13 +139,13 @@ def main():
         # cooldowns 
 
         if red_stun == False:
-            if red_stun_cd2 - red_stun_cd >= 1000:
+            if red_stun_cd2 - red_stun_cd >= 500:
                 red_stun = True
                 red_stun_cd = red_stun_cd2
                 
         
         if blue_stun == False:
-            if blue_stun_cd2 - blue_stun_cd >= 1000:
+            if blue_stun_cd2 - blue_stun_cd >= 500:
                 blue_stun = True
                 blue_stun_cd = blue_stun_cd2
 
@@ -159,16 +159,18 @@ def main():
             red_cd = red_cd2
             red_frame = 0
 
-        if blue.x >=  980:
+        if blue.x >= 980:
             blue.x = 980
 
         if red.x <= 0:
-            red.x =0 
-        if keys_pressed[pygame.K_l] and blue_stun == True: #todo add a feature that adds CD to if pressed early and stun if hit 
+            red.x = 0 
+
+        if keys_pressed[pygame.K_l] and blue_stun == True and blue_able == True: #todo add a feature that adds CD to if pressed early and stun if hit 
             blue_stun = False
+            blue_able = True 
             blue_cd2 = pygame.time.get_ticks()
             blue_frame = 2 
-            blue_attack = pygame.draw.rect(WINDOW, (0,0,225), pygame.Rect(blue.x - 20, blue.y, 10, 300))
+            blue_attack = pygame.Rect(blue.x - 20, blue.y, 10, 300)
             if blue_attack.colliderect(red):
                 punch_sound.play()
                 red_hp -= 10
@@ -182,7 +184,7 @@ def main():
             red_stun = False 
             red_cd2 = pygame.time.get_ticks()
             red_frame = 2
-            red_attack = pygame.draw.rect(WINDOW, (255,0,0), pygame.Rect(red.x + 170, red.y, 10, 300))
+            red_attack = pygame.Rect(red.x + 170, red.y, 10, 300)
             if  red_attack.colliderect(blue):
                 punch_sound.play()
                 blue_hp -=10
@@ -255,6 +257,7 @@ def ai():
     #! animations 
     blue_last_update = pygame.time.get_ticks()
     red_last_update = pygame.time.get_ticks()
+
     blue_animation_cooldown, red_animation_cooldown= 150, 150
     blue_frame = 0 
     red_frame = 0
@@ -268,13 +271,11 @@ def ai():
 
     red_stun = True 
     blue_stun = True
-
-    blue_ko = False 
-    red_ko = False 
     
 
     red_stun_cd = pygame.time.get_ticks() 
     blue_stun_cd = pygame.time.get_ticks()
+
 
 
 
@@ -298,13 +299,13 @@ def ai():
         # cooldowns 
 
         if red_stun == False:
-            if red_stun_cd2 - red_stun_cd >= 1000:
+            if red_stun_cd2 - red_stun_cd >= 500:
                 red_stun = True
                 red_stun_cd = red_stun_cd2
                 
         
         if blue_stun == False:
-            if blue_stun_cd2 - blue_stun_cd >= 1000:
+            if blue_stun_cd2 - blue_stun_cd >= 500:
                 blue_stun = True
                 blue_stun_cd = blue_stun_cd2
 
@@ -318,39 +319,50 @@ def ai():
             red_cd = red_cd2
             red_frame = 0
 
-        if blue.x >=  980:
+        if blue.x >= 980:
             blue.x = 980
 
         if red.x <= 0:
-            red.x =0 
-        if keys_pressed[pygame.K_l] and blue_stun == True and red_ko == False: #todo add a feature that adds CD to if pressed early and stun if hit 
+            red.x = 0
+
+        if keys_pressed[pygame.K_l] and blue_stun == True and blue_able == True: #todo add a feature that adds CD to if pressed early and stun if hit 
             blue_stun = False
+            blue_able = True 
             blue_cd2 = pygame.time.get_ticks()
             blue_frame = 2 
-            blue_attack = pygame.draw.rect(WINDOW, (0,0,225), pygame.Rect(blue.x - 20, blue.y, 10, 300))
+            blue_attack = pygame.Rect(blue.x - 20, blue.y, 10, 300)
             if blue_attack.colliderect(red):
                 punch_sound.play()
                 red_hp -= 10
                 red_cd2 = 0
-                red_able == False
+                red_stun == False
                 blue_attack.y+=500
                 red.x-=60
 
 
-        if blue.x <= red.x + 230 and red_stun == True and blue_ko == False:
+        if blue.x <= red.x + 170 and red_stun == True:
             red_stun = False 
             red_cd2 = pygame.time.get_ticks()
             red_frame = 2
-            red_attack = pygame.draw.rect(WINDOW, (255,0,0), pygame.Rect(red.x + 170, red.y, 10, 300))
-            if  red_attack.colliderect(blue):
+            red_attack = pygame.Rect(red.x + 170, red.y, 10, 300)
+            if red_attack.colliderect(blue):
                 punch_sound.play()
                 blue_hp -=10
                 blue.x+= 60
                 blue_cd2 = 0
                 blue_stun == False 
                 red_attack.y += 500
+
+
+        if red.x <= blue.x - 20 and red.x + VELOCITY < blue.x - 90 and red.x-VELOCITY<= 1080 and red_stun == True:
+            red.x += VELOCITY 
+            if red_current_time - red_last_update >= red_animation_cooldown:
+                red_frame +=1 
+                red_last_update = red_current_time
+                if red_frame >= 2: 
+                    red_frame = 0
             
-        if keys_pressed[pygame.K_LEFT] and blue.x + VELOCITY >= red.x + 115 and blue_stun == True and red_ko == False:
+        if keys_pressed[pygame.K_LEFT] and blue.x + VELOCITY >= red.x + 115 and blue_stun == True:
             blue.x -= VELOCITY
             if blue_current_time - blue_last_update >= blue_animation_cooldown:
                 blue_frame +=1 
@@ -358,7 +370,7 @@ def ai():
                 if blue_frame >= 2:
                     blue_frame = 0 
 
-        if keys_pressed[pygame.K_RIGHT] and blue.x - VELOCITY!= 980  and red_ko == False:
+        if keys_pressed[pygame.K_RIGHT] and blue.x - VELOCITY!= 980 and blue_stun == True:
             blue.x += VELOCITY
             if blue_current_time - blue_last_update >= blue_animation_cooldown:
                 blue_frame +=1 
@@ -366,8 +378,7 @@ def ai():
                 if blue_frame >= 2:
                     blue_frame = 0 
 
-
-        if red.x <= blue.x - 20 and red.x + VELOCITY < blue.x - 90 and red.x-VELOCITY<= 1080 and red_stun == True and blue_ko == False:
+        if keys_pressed[pygame.K_d] and red.x + VELOCITY < blue.x - 90 and red.x-VELOCITY<= 1080 and red_stun == True:
             red.x += VELOCITY 
             if red_current_time - red_last_update >= red_animation_cooldown:
                 red_frame +=1 
@@ -375,31 +386,35 @@ def ai():
                 if red_frame >= 2: 
                     red_frame = 0 
 
+        if keys_pressed[pygame.K_a] and red.x !=0 and red_stun == True:
+            red.x -= VELOCITY 
+            if red_current_time - red_last_update >= red_animation_cooldown:
+                red_frame +=1 
+                red_last_update = red_current_time
+                if red_frame >= 2:
+                  red_frame = 0  
         
         winner_text =  '' 
 
         if blue_hp <=0:
             winner_text = 'RED WINS '
-            blue_ko = True
             text_colour = (150,0,0)
         
         if red_hp <= 0:
             winner_text = 'BLUE WINS'
             text_colour = (0,0,150)
 
-        
+        draw_window(blue, red, blue_frame, red_frame, blue_hp, red_hp, blue_healthbar)
+
         if winner_text != '': 
             draw_winner(winner_text, text_colour)
             pygame.time.delay(3000)
             run = False
             menu()
 
-        draw_window(blue, red, blue_frame, red_frame, blue_hp, red_hp, blue_healthbar)
-
-        if winner_text != '':
-            draw_winner(winner_text, text_colour)
 
     pygame.quit() #? if loop breaks then exit the pygame
+
 #! main menu 
 #window 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
